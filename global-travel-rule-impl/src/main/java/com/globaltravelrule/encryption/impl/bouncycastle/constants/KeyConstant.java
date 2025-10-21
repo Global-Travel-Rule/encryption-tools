fa31748f4824dc965e20f1f4ff7a4124c1e4bfba
Global Travel Rule Developer <developer@globaltravelrule.com>
2025-10-21 10:25:22 +0700

    feature: fix PEM certificate String begin and end (#6)

---
+++ global-travel-rule-impl/src/main/java/com/globaltravelrule/encryption/impl/bouncycastle/constants/KeyConstant.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022-2025 Global Travel Rule • globaltravelrule.com
+ * License that can be found in the LICENSE file.
+ * Author: Global Travel Rule developer
+ * Created on: 2025/10/21 09:19
+ */
+
+package com.globaltravelrule.encryption.impl.bouncycastle.constants;
+
+/**
+ * key constant
+ *
+ * @author Global Travel Rule developer
+ * @version 1.0
+ * @since 2025/10/21 09:19
+ *
+ */
+public interface KeyConstant {
+
+    //new line length for PEM format
+    int PEM_NEW_LINE_LENGTH = 64;
+}
